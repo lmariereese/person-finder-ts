cf33d2b918118bdc7ec40dcc8b8e956110760739
Lyndsey Reese <lmariereese@gmail.com>
2021-01-18 10:20:34 -0500

    Styles: Added all styles

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,4 +1,6 @@
+import {default as AppHeader} from './AppHeader';
+import {default as Main} from './Main';
 import {default as PersonFinder} from './PersonFinder';
 import {default as PersonCard} from './PersonCard';
 
-export {PersonFinder, PersonCard}
+export {AppHeader, Main, PersonFinder, PersonCard}
